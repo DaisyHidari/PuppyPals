58cf02f05b156d76bd91e9e62ab260b94e945c18
Desiree <chuchu930716@gmail.com>
2024-03-28 00:04:45 -0700

    debugged

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { puppyList } from './data.js'
 import './App.css'
 
@@ -8,7 +6,6 @@ import './App.css'
 function App() {
 
   // const [puppies, setPuppies] = useState(puppyList);
-
   const [featPupId, setFeatPupId] = useState(null)
 
   const handleClick = (puppyId) => {
@@ -24,28 +21,24 @@ function App() {
   return (
     <>
       <div className="App">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-
-        {puppyList.map((puppy) => {
-        return (
-          <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
-          {puppy.name}
-        </p>
-        );
-      })}
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
+        <div className="puppy-list">
+          {puppyList.map((puppy) => (
+            <p key={puppy.id} onClick={() => handleClick(puppy.id)} className="puppy-name">
+              {puppy.name}
+            </p>
+          ))}
         </div>
-      </div>
+        {featPupId && (
+          <div className="featured-puppy">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+        </div>
+      
       
       
     </>

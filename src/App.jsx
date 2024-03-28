6b266a35de0a6b1781def30be369b4bf9c318910
Desiree <chuchu930716@gmail.com>
2024-03-27 22:57:37 -0700

    added some content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log("puppyList: ", puppyList);
 
   return (
     <>
@@ -13,6 +20,12 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+
       </div>
       
       
